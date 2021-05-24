7f5a5e260921534fea390ea0ecc468844ea29f38
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-05-24 19:31:05 +0530

    Lazy loading sample has been added

---
+++ LazyLoading/LazyLoading/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Syncfusion.XForms.TabView;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LazyLoading
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new TabViewModel();
+            this.simTab.SelectionIndicatorSettings = new SelectionIndicatorSettings() { Position = SelectionIndicatorPosition.Fill, Color = Color.FromHex("#FF00AFF0") };
+            this.ContactTab1.SelectionIndicatorSettings = this.ContactTab2.SelectionIndicatorSettings = this.CallsTab1.SelectionIndicatorSettings = this.CallTab2.SelectionIndicatorSettings = new SelectionIndicatorSettings() { Position = SelectionIndicatorPosition.Top };
+        }
+
+    }
+
+    public class CustomViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Device.RuntimePlatform == "Android")
+            {
+                if (parameter != null && parameter is string)
+                    return "NestedTab.ttf#" + parameter.ToString();
+                else
+                    return "NestedTab.ttf";
+            }
+            else if (Device.RuntimePlatform == "iOS")
+            {
+
+                return "NestedTab";
+
+            }
+            else
+            {
+                var ff= "/Assets/NestedTab.ttf#NestedTab";
+               var nested = "NestedTab.ttf#NestedTab";
+                return ff;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+  }
---
+++ LazyLoading/LazyLoading/Model/ContactsInfo.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LazyLoading
+{
+    public class ContactsInfo : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string contactName;
+        private string contactNo;
+        private string image;
+        private string contactType;
+        private string date;
+        private string message;
+        private bool read = false;
+        private string messageCount;
+        private string dateMonth;
+
+        #endregion
+
+        #region Constructor
+
+        public ContactsInfo()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public string ContactName
+        {
+            get { return this.contactName; }
+            set
+            {
+                this.contactName = value;
+                RaisePropertyChanged("ContactName");
+            }
+        }
+
+        public string ContactNumber
+        {
+            get { return contactNo; }
+            set
+            {
+                this.contactNo = value;
+                RaisePropertyChanged("ContactNumber");
+            }
+        }
+
+        public string ContactReadType
+        {
+            get { return contactType; }
+            set
+            {
+                this.contactType = value;
+                RaisePropertyChanged("ContactReadType");
+            }
+        }
+
+        public string ContactImage
+        {
+            get { return this.image; }
+            set
+            {
+                this.image = value;
+                this.RaisePropertyChanged("ContactImage");
+            }
+        }
+
+        public string Message
+        {
+            get { return this.message; }
+            set
+            {
+                this.message = value;
+                this.RaisePropertyChanged("Message");
+            }
+        }
+
+        public bool Read
+        {
+            get { return this.read; }
+            set
+            {
+                this.read = value;
+                this.RaisePropertyChanged("Read");
+            }
+        }
+
+        public string Date
+        {
+            get { return this.date; }
+            set
+            {
+                this.date = value;
+                this.RaisePropertyChanged("Date");
+            }
+        }
+        public string DateMonth
+        {
+            get { return this.dateMonth; }
+            set
+            {
+                this.dateMonth = value;
+                this.RaisePropertyChanged("DateMonth");
+            }
+        }
+        public string MessageCount
+        {
+            get { return this.messageCount; }
+            set
+            {
+                this.messageCount = value;
+                this.RaisePropertyChanged("MessageCount");
+            }
+        }
+
+
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
